749bb1dde89a98e2f2db2bcfe4a1732aae85836b
Choi Hyun Kyung <hkchoi@nhntoast.com>
2020-12-30 12:34:04 +0900

    feat : 프로젝트 생성

---
+++ src/test/java/com/modern/java/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modern.java.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
